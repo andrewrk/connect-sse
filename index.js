85f2f6cf7f64c193f41c15ff8b0f9cd94a8a72c8
Andrew Kelley <superjoe30@gmail.com>
2012-12-30 01:55:30 -0500

    Revert "send headers immediately"
    This reverts commit 85d15df98f8c263d4b83cd8d9f4c276ae7f087ca.

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = function () {
   return function sse(req, resp, next){
+    var message_count;
     req.socket.setTimeout(Infinity);
-    resp.writeHead(200, {
-      'Content-Type': 'text/event-stream',
-      'Cache-Control': 'no-cache',
-      'Connection': 'keep-alive',
-    });
-    var messageCount = 0;
+    resp.statusCode = 200;
+    resp.setHeader('Content-Type', 'text/event-stream');
+    resp.setHeader('Cache-Control', 'no-cache');
+    resp.setHeader('Connection', 'keep-alive');
+    message_count = 0;
     resp.json = function(obj){
-      resp.write("id: " + messageCount + "\n");
+      resp.write("id: " + message_count + "\n");
       resp.write("data: " + JSON.stringify(obj) + "\n\n");
-      messageCount += 1;
+      message_count += 1;
     };
     next();
   };
